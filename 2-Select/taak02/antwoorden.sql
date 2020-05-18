7e771b1e6fd37ef4d5f1e666898d9951ac396451
Randy Ofosu <randy_ofosu@hotmail.com>
2020-05-18 14:44:14 +0200

    MySQL select 2

--- 2-Select/taak02/antwoorden.sql
+++ 2-Select/taak02/antwoorden.sql
@@ -1,19 +1,19 @@
 -- Opdracht 1 
-
+SELECT * FROM jaar2015 WHERE rank = 7;
 -- Opdracht 2 
-
+SELECT * FROM jaar2016 WHERE country = "Netherlands";
 -- Opdracht 3
-
+SELECT score FROM jaar2016 WHERE country = "Netherlands";
 -- Opdracht 4
-
+SELECT * FROM jaar2015 WHERE region = "North America";
 -- Opdracht 5
-
+SELECT country FROM jaar2016 WHERE rank = 25;
 -- Opdracht 6 
-
+SELECT country FROM jaar2016 WHERE score > 7087;
 -- Opdracht 7 
-
+SELECT country, region FROM jaar2016 WHERE rank <= 5;
 -- Opdracht 8
-
+SELECT score FROM jaar2016 WHERE country = "Ireland";
 -- Opdracht 9
-
+SELECT country FROM jaar2016 WHERE region = "Latin America and Caribbean";
 -- Opdracht 10
