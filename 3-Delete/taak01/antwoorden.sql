881b2ad7016d5a3caf5f762eb0b1ba439d59fd63
Randy Ofosu <randy_ofosu@hotmail.com>
2020-05-19 12:09:19 +0200

    MySQL delete opdracht 1

--- 3-Delete/taak01/antwoorden.sql
+++ 3-Delete/taak01/antwoorden.sql
@@ -1,9 +1,10 @@
 -- Opdracht 1 
-
+DELETE FROM players WHERE name = "David Silva";
 -- Opdracht 2 
-
+DELETE FROM players WHERE club = "Willem II";
 -- Opdracht 3
-
+DELETE FROM players WHERE nationality = "Belgium" AND club = "FC Barcelona";
 -- Opdracht 4
-
+DELETE from players where age >= 38
 -- Opdracht 5
+DELETE from players where id = 167905 OR id = 169595
